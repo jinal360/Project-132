c2efc30d7a46ee0abec06592317e162a016aa85f
jinal360 <73110780+jinal360@users.noreply.github.com>
2021-08-30 19:04:46 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -160,48 +160,5 @@ for planet_data in low_gravity_planets:
 fig = px.scatter(x=planet_radiuses, y=planet_masses)
 fig.show()
 
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-X = []
-for index, planet_mass in enumerate(planet_masses):
-  temp_list = [
-                  planet_radiuses[index],
-                  planet_mass
-              ]
-  X.append(temp_list)
-
-wcss = []
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters=i, init='k-means++', random_state = 42)
-    kmeans.fit(X)
-    wcss.append(kmeans.inertia_)
-
-plt.figure(figsize=(10,5))
-sns.lineplot(range(1, 11), wcss, marker='o', color='red')
-plt.title('The Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
-
-X = []
-for index, planet_mass in enumerate(planet_masses):
-  temp_list = [
-                  planet_gravity[index],
-                  planet_mass
-              ]
-  X.append(temp_list)
-
-wcss = []
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters=i, init='k-means++', random_state = 42)
-    kmeans.fit(X)
-    wcss.append(kmeans.inertia_)
-
-plt.figure(figsize=(10,5))
-sns.lineplot(range(1, 11), wcss, marker='o', color='red')
-plt.title('The Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
+fig = px.scatter(x=planet_gravity, y=planet_masses)
+fig.show()
